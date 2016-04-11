788f4f29cc103fff08499b77fba238ccaef91798
Dmitry Ermakov <dmitry.ermakov@lazada.com>
2016-04-12 01:24:10 +0700

    Show more

--- frontend/components/more-list/index.js
+++ frontend/components/more-list/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import MoreItem from '~/components/more-item';
 import '~/components/more-list/styles.css';
 
+const SHOW_NUM = 20;
+
 export default class MoreList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            photosToShow: SHOW_NUM
+        }
+    }
+
+    updateNumberToShow = () => {
+        this.setState({
+            photosToShow: this.state.photosToShow + SHOW_NUM
+        });
+    }
 
     render() {
+        const showData = this.props.data.slice(0, this.state.photosToShow);
         return (
-            <div className="more__list">
+            <div>
                 <div className="more-list">
-                    {this.props.data.map((image, index) => {
+                    {showData.map((image, index) => {
                         return <MoreItem image={image}  key={index} />;
                     })}
+                </div>
+                <div
+                    style={{display:(this.props.data.length > this.state.photosToShow) ? "block" : "none"}}
+                    className="more-list__show"
+                    onClick={this.updateNumberToShow}>
+                    Show more..
                 </div>
             </div>
         );
